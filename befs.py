2e09efc45478a9e1f12fb0f950e9a8a154502380
randommessi10 <smehcodes@gmail.com>
2026-01-08 19:13:03 +0530

    Update befs function to return path to goal

--- befs.py
+++ befs.py
@@ -1,16 +1,23 @@
 def befs(graph, start, goal, heuristic):
     visited = {}
-    queue = [(heuristic[start], start)]
+    queue = [(heuristic[start], start, [start])]
+
     while queue:
-        queue.sort()
-        cost, node = queue.pop(0)
+        queue.sort()   # lowest heuristic first
+        cost, node, path = queue.pop(0)
+
         if node == goal:
-            print("Goal found", node)
-            return True
+            print("Goal found")
+            return path
+
         if node not in visited:
             visited[node] = True
+
             for neighbor in graph[node]:
                 if neighbor not in visited:
-                    queue.append((heuristic[neighbor], neighbor))
+                    queue.append(
+                        (heuristic[neighbor], neighbor, path + [neighbor])
+                    )
+
     print("Goal not found")
-    return False
+    return None
